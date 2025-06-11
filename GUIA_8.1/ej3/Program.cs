37922404c6ae02f564c3915ebd687363277c6000
adlfvrr <adolfover4@gmail.com>
2025-06-11 01:07:43 -0300

    commit

---
+++ GUIA_8.1/ej3/Program.cs
@@ -0,0 +1,92 @@
+﻿using ej3_servicio;
+namespace ej3
+{
+    internal class Program
+    {
+        static private Servicio servicio = new Servicio();
+        static private int MostrarPantallaSolicitarOpcionMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Ingrese opción: \n1.Registrar numeros de libreta\n2.Mostrar lista ordenada\nOtro.Salir");
+            int op = Convert.ToInt32(Console.ReadLine());
+            return op;
+        }
+        static private void MostrarPantallaSolicitarAlumnos()
+        {
+            servicio.RegistrarNombreYNumeroLibreta();
+        }
+        static private void MostrarPantallaMostrarListaOrdenada()
+        {
+            if (servicio.NumeroLibreta0 > servicio.NumeroLibreta1)
+            {
+                if (servicio.NumeroLibreta0 > servicio.NumeroLibreta2)
+                {
+                    if (servicio.NumeroLibreta1 > servicio.NumeroLibreta2)
+                    {
+                        Console.WriteLine(@$"El alumno con mayor numero es {servicio.Nombre0} con el numero {servicio.NumeroLibreta0}
+El alumno con numero medio es {servicio.Nombre1} con la nota {servicio.NumeroLibreta1}
+El alumno con menor numero es {servicio.Nombre2} con el numero {servicio.NumeroLibreta2}");
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        Console.WriteLine(@$"El alumno con mayor numero es {servicio.Nombre0} con el numero {servicio.NumeroLibreta0}
+El alumno con numero medio es {servicio.Nombre2} con la nota {servicio.NumeroLibreta2}
+El alumno con menor numero es {servicio.Nombre1} con el numero {servicio.NumeroLibreta1}");
+                        Console.ReadKey();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(@$"El alumno con mayor numero es {servicio.Nombre2} con el numero {servicio.NumeroLibreta2}
+El alumno con numero medio es {servicio.Nombre0} con la nota {servicio.NumeroLibreta0}
+El alumno con menor numero es {servicio.Nombre1} con el numero {servicio.NumeroLibreta1}");
+                    Console.ReadKey();
+                }
+            }
+            else
+            {
+                if (servicio.NumeroLibreta1 > servicio.NumeroLibreta2)
+                {
+                    if (servicio.NumeroLibreta2 > servicio.NumeroLibreta0)
+                    {
+                        Console.WriteLine(@$"El alumno con mayor numero es {servicio.Nombre1} con el numero {servicio.NumeroLibreta1}
+El alumno con numero medio es {servicio.Nombre2} con la nota {servicio.NumeroLibreta2}
+El alumno con menor numero es {servicio.Nombre0} con el numero {servicio.NumeroLibreta0}");
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        Console.WriteLine(@$"El alumno con mayor numero es {servicio.Nombre1} con el numero {servicio.NumeroLibreta1}
+El alumno con numero medio es {servicio.Nombre0} con la nota {servicio.NumeroLibreta0}
+El alumno con menor numero es {servicio.Nombre2} con el numero {servicio.NumeroLibreta2}");
+                        Console.ReadKey();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(@$"El alumno con mayor numero es {servicio.Nombre2} con el numero {servicio.NumeroLibreta2}
+El alumno con numero medio es {servicio.Nombre1} con la nota {servicio.NumeroLibreta1}
+El alumno con menor numero es {servicio.Nombre0} con el numero {servicio.NumeroLibreta0}");
+                    Console.ReadKey();
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            bool menu = true;
+            while (menu)
+            {
+                int op = MostrarPantallaSolicitarOpcionMenu();
+                switch (op)
+                {
+                    case 1: MostrarPantallaSolicitarAlumnos(); break;
+                    case 2: MostrarPantallaMostrarListaOrdenada(); break;
+                    default: menu = !menu; break;
+                }
+            }
+        }
+    }
+}
+    
+
---
+++ GUIA_8.1/ej3/Servicio.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ej3_servicio
+{
+    internal class Servicio
+    {
+        public string Nombre0, Nombre1, Nombre2;
+        public int NumeroLibreta0, NumeroLibreta1, NumeroLibreta2;
+        private int Orden;
+        public Servicio()
+        {
+            Nombre0 = ""; Nombre1 = ""; Nombre2 = ""; NumeroLibreta0 = 0; NumeroLibreta1 = 0; NumeroLibreta2 = 0; Orden = 0;
+        }
+        public void RegistrarNombreYNumeroLibreta()
+        {
+            if (Orden < 4)
+            {
+                Console.Write($"Ingrese nombre del alumno {Orden + 1}: ");
+                string nombre = Console.ReadLine();
+                Console.Write("Ingrese número de libreta: ");
+                int numero = Convert.ToInt32(Console.ReadLine());
+                if (Nombre0 == "" && NumeroLibreta0 == 0)
+                {
+                    Nombre0 = nombre;
+                    NumeroLibreta0 = numero;
+                }
+                else if (Nombre1 == "" && NumeroLibreta1 == 0)
+                {
+                    Nombre1 = nombre;
+                    NumeroLibreta1 = numero;
+                }
+                else if (Nombre2 == "" && NumeroLibreta2 == 0)
+                {
+                    Nombre2 = nombre;
+                    NumeroLibreta2 = numero;
+                }
+                Orden++;
+            }
+            else
+            {
+                Console.WriteLine("Límite de ingresos alcanzado.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
