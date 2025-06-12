43fdf63af70d18245c98d6f0cb7f6d59ab43b89f
adlfvrr <adolfover4@gmail.com>
2025-06-12 00:30:29 -0300

    commit

---
+++ GUIA_8.1/ej7/Models/Servicio.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ej7.Models
+{
+    internal class Servicio
+    {
+        private int cantidad1, cantidad2, cantidad3, cantidad4, cantidad5, contadorDeTransacciones;
+        public int NumeroTransaccionMayor;
+        public double MontoTransaccionMayor, PorcentajeCantidadRubro1, PorcentajeCantidadRubro2, PorcentajeCantidadRubro3, PorcentajeCantidadRubro4, PorcentajeCantidadRubro5, RecaudacionTotal;
+        public Servicio()
+        {
+            cantidad1 = 0; cantidad2 = 0; cantidad3 = 0; cantidad4= 0; cantidad5= 0; contadorDeTransacciones = 0; NumeroTransaccionMayor = 0; MontoTransaccionMayor= 0;PorcentajeCantidadRubro1 = 0; PorcentajeCantidadRubro2 = 0; PorcentajeCantidadRubro3 = 0; PorcentajeCantidadRubro4 = 0; PorcentajeCantidadRubro5   = 0; RecaudacionTotal = 0;
+        }
+        public void InicializarVariables()
+        {
+            cantidad1 = 0; cantidad2 = 0; cantidad3 = 0; cantidad4 = 0; cantidad5 = 0; contadorDeTransacciones = 0; NumeroTransaccionMayor = 0; MontoTransaccionMayor = 0; PorcentajeCantidadRubro1 = 0; PorcentajeCantidadRubro2 = 0; PorcentajeCantidadRubro3 = 0; PorcentajeCantidadRubro4 = 0; PorcentajeCantidadRubro5 = 0; RecaudacionTotal = 0;
+        }
+        public void EvaluarTransaccionPuntoDeVenta(int nroTransaccion, int rubro, int cantidad, double monto)
+        {
+            switch (rubro)
+            {
+                case 1: cantidad1 += cantidad; RecaudacionTotal += monto; break;
+                case 2: cantidad2 += cantidad; RecaudacionTotal += monto; break;
+                case 3: cantidad3 += cantidad; RecaudacionTotal += monto; break;
+                case 4: cantidad4 += cantidad; RecaudacionTotal += monto; break;
+                case 5: cantidad5 += cantidad; RecaudacionTotal += monto; break;
+                default: Console.WriteLine("Rubro inexistente"); break;
+            }
+            if (NumeroTransaccionMayor == 0 && MontoTransaccionMayor == 0)
+            {
+                NumeroTransaccionMayor = nroTransaccion;
+                MontoTransaccionMayor = monto;
+            }
+            else
+            {
+                if (monto > MontoTransaccionMayor)
+                {
+                    MontoTransaccionMayor = monto;
+                    NumeroTransaccionMayor = nroTransaccion;
+                }
+            }
+        }
+        public void CalcularPorcentajesCantidadVentasPorRubro(int ContadorTransaccionesProgram)
+        {
+            PorcentajeCantidadRubro1 = 1.0 * (cantidad1 / (double)ContadorTransaccionesProgram) * 100;
+            PorcentajeCantidadRubro2 = 1.0 * (cantidad2 / (double)ContadorTransaccionesProgram) * 100;
+            PorcentajeCantidadRubro3 = 1.0 * (cantidad3 / (double)ContadorTransaccionesProgram) * 100;
+            PorcentajeCantidadRubro4 = 1.0 * (cantidad4 / (double)ContadorTransaccionesProgram) * 100;
+            PorcentajeCantidadRubro5 = 1.0 * (cantidad5 / (double)ContadorTransaccionesProgram) * 100;
+        }
+    }
+}
---
+++ GUIA_8.1/ej7/Program.cs
@@ -0,0 +1,93 @@
+﻿using ej7.Models;
+namespace ej7
+{
+    internal class Program
+    {
+        private static Servicio servicio = new Servicio();
+        private static int MostrarPantallaSolicitarOpcionMenu()
+        {
+            {
+                Console.Clear();
+                Console.WriteLine(@"Ingrese opción: 
+1- Ingresar un resumen de venta.
+2- Mostrar Número de transacción registrado con el mayor monto total.
+3- Mostrar porcentaje de cantidad por rubro.
+4- Mostrar la recaudación total.
+Otro- Salir");
+                int opcion = Convert.ToInt32(Console.ReadLine());
+                return opcion;
+            }
+        }
+        private static void MostrarPantallaRegistrarTransaccion()
+        {
+            Console.Write("Ingrese numero de transacción: ");
+            int nroTransaccion = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingrese rubro: ");
+            int rubro = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingrese cantidad: ");
+            int cantidad = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingrese monto: ");
+            double monto = Convert.ToDouble(Console.ReadLine());
+            servicio.EvaluarTransaccionPuntoDeVenta(nroTransaccion, rubro, cantidad, monto);
+        }
+        private static void MostrarPantallaPorcentajeDeCantidadesPorRubro(int contadorDeTransacciones)
+        {
+            if (contadorDeTransacciones > 0)
+            {
+                servicio.CalcularPorcentajesCantidadVentasPorRubro(contadorDeTransacciones);
+                Console.WriteLine(@$"Los porcentajes de cantidad por rubro son:
+Rubro 1: {servicio.PorcentajeCantidadRubro1:f2}%
+Rubro 2: {servicio.PorcentajeCantidadRubro2:f2}%
+Rubro 3: {servicio.PorcentajeCantidadRubro3:f2}%
+Rubro 4: {servicio.PorcentajeCantidadRubro4:f2}%
+Rubro 5: {servicio.PorcentajeCantidadRubro5:f2}%");
+                Console.ReadKey();
+            }
+            else { Console.WriteLine("No hubo transacciones."); Console.ReadKey(); }
+        }
+        private static void MostrarPantallaTransaccionMayorMonto(int contadorDeTransacciones)
+        {
+            if (contadorDeTransacciones > 0)
+            {
+                Console.WriteLine($"La transaccion con mayor monto es la numero {servicio.NumeroTransaccionMayor}, con un monto de ${servicio.MontoTransaccionMayor:f2}");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("No hubo transacciones.");
+                Console.ReadKey();
+            }
+        }
+        private static void MostrarPantallaMontoRecaudadoTotal(int contadorDeTransacciones)
+        {
+            if (contadorDeTransacciones > 0)
+            {
+                Console.WriteLine($"La recaudación total es de ${servicio.RecaudacionTotal:f2}");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("No hubo transacciones");
+                Console.ReadKey();
+            }
+        }
+        static void Main(string[] args)
+        {
+            int contadorDeTransacciones = 0;
+            bool menu = true;
+            while (menu)
+            {
+                int op = MostrarPantallaSolicitarOpcionMenu();
+                switch (op)
+                {
+                    case 1: MostrarPantallaRegistrarTransaccion(); contadorDeTransacciones++; break;
+                    case 2: MostrarPantallaTransaccionMayorMonto(contadorDeTransacciones); break;
+                    case 3: MostrarPantallaPorcentajeDeCantidadesPorRubro(contadorDeTransacciones); break;
+                    case 4: MostrarPantallaMontoRecaudadoTotal(contadorDeTransacciones); break;
+                    default: menu = false; break;
+                }
+
+            }
+        }
+    }
+}
